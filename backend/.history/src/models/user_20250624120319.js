fc420cb27e5ddb782cb9e00aed18a4e1940dd967
Lean Gianotto <leandrogianotto@gmail.com>
2025-07-03 10:11:45 -0300

    Consultar estado mis pedidos

--- backend/.history/src/models/user_20250624120319.js
+++ backend/.history/src/models/user_20250624120319.js
@@ -4,10 +4,7 @@ const sequelize = require('../config/db');
 const User = sequelize.define('User', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, 
-    allowNull: false, 
-    // unique: true 
-    },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
   role: { type: DataTypes.STRING, defaultValue: 'user' } // 'user' o 'admin'
 }, { timestamps: false });
